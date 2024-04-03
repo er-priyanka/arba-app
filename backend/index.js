3e091f27eec41dfaf3daf81adb7c42e970c4695b
er-priyanka <shahanepriyankas01@gmail.com>
2024-04-03 23:01:17 +0530

    initialized setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require('express');
+const { connection } = require("./Configs/db");
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(PORT, async () => {
+    try{
+        await connection;
+        console.log("Connected to db");
+    }catch(err){
+        console.log(err);
+    }
+    console.log(`server started on http://localhost:${PORT}`)
+
+});
