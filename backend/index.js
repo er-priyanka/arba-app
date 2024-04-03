bb252615709578ffe29e26c8913cca720674af9f
er-priyanka <shahanepriyankas01@gmail.com>
2024-04-03 23:39:54 +0530

    added cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require('express');
 const { connection } = require("./Configs/db");
+const cors = require('cors');
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
